381155f7bffb9ec2dd7b75cbcbac30d86fda8698
ismet <ismet_dayan@outlook.com>
2022-03-17 02:33:04 +0300

    Initial Commit

---
+++ SinavSistemi.DataAccessLayer/Baglanti.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinavSistemi.DataAccessLayer
+{
+    class Baglanti
+    {
+        private static Baglanti baglanti;
+        static object kilitliNesne = new object();
+
+        private Baglanti() { }
+
+        public static Baglanti TekNesneYarat()
+        {
+            lock (kilitliNesne)
+            {
+                if (baglanti == null)
+                    baglanti = new Baglanti();
+            }
+            return baglanti;
+        }
+
+        SqlConnection baglantiKablosu;
+        public SqlConnection BaglantiKablosu
+        {
+            get
+            {
+                if (baglantiKablosu != null)
+                {
+                    if (baglantiKablosu.State == ConnectionState.Open)
+                    {
+                        baglantiKablosu.Close();
+                        baglantiKablosu.Open();
+                    }
+                    else
+                    {
+                        baglantiKablosu.Open();
+                    }
+                    return baglantiKablosu;
+                }
+                else
+                {
+                    baglantiKablosu = new SqlConnection(Provider());
+                    if (baglantiKablosu.State == ConnectionState.Closed)
+                    {
+                        baglantiKablosu.Open();
+                    }
+                    return baglantiKablosu;
+                }
+            }
+        }
+
+        private string Provider()
+        {
+            return ConfigurationManager.AppSettings["BaglantiCumlesi"];
+        }
+
+
+    }
+}
---
+++ SinavSistemi.Entity/Soru.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinavSistemi.Entity
+{
+    public class Soru : IDisposable
+    {
+        public int SoruID { get; set; }
+        public string SoruMetni { get; set; }
+        public int KonuID { get; set; }
+        public int UniteID { get; set; }
+        public string ResimYolu { get; set; }
+        public string DogruCevap { get; set; }
+        public string YanlisCevaplar { get; set; }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
